ba00a1c99b80548972c6c1fd69e8467ebe0abf2c
Christopher West <9ninemind@gmail.com>
2015-04-04 14:58:05 -0600

    creates bundle

---
+++ BlackBoxCodePandoBundleAccountTicketBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BlackBoxCode\Pando\Bundle\AccountTicketBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BlackBoxCodePandoBundleAccountTicketBundle extends Bundle
+{
+}
