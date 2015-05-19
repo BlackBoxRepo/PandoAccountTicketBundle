3c20aff6836ae8d6e4b252a0cf011a55c2bebd32
Christopher West <9ninemind@gmail.com>
2015-05-19 11:19:23 -0600

    adds BundleException

---
+++ Exception/AccountTicketBundleException.php
@@ -0,0 +1,9 @@
+<?php
+namespace BlackBoxCode\Pando\AccountTicketBundle\Exception;
+
+use BlackBoxCode\Pando\BaseBundle\Exception\BaseException;
+
+class AccountTicketBundleException extends BaseException
+{
+
+}
